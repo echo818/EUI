f6a5e8228053989ea82124abd4f3a958b677d471
echo008 <1067054231@qq.com>
2016-05-12 10:02:31 +0800

    watch service

--- app/src/main/java/com/echo/eui/service/CountService.java
+++ app/src/main/java/com/echo/eui/service/CountService.java
@@ -8,19 +8,34 @@ import android.os.IBinder;
  * Created by Echo-z on 2016/5/11/0011.
  */
 public class CountService extends Service {
+    private int i = 0;
 
     @Override
     public IBinder onBind(Intent intent) {
+        System.out.println("onBind:" + intent);
         return null;
     }
 
     @Override
     public void onCreate() {
+        System.out.println("Service process:" + android.os.Process.myPid());
+        System.out.println("Service Thread:" + Thread.currentThread().getId());
+        System.out.println("onCreate");
         super.onCreate();
+//        int n = 11;
+//        while (n > 0) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            n--;
+//            System.out.println(n);
+//        }
         new Thread(new Runnable() {
             @Override
             public void run() {
-                int i = 0;
+
                 while (i < 20) {
                     try {
                         Thread.sleep(1000);
@@ -32,5 +47,36 @@ public class CountService extends Service {
                 }
             }
         }).start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("onStartCommand:" + intent + "   flags:" + flags + "   startId:" + startId);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        System.out.println("onStart:" + intent + "   startId:" + startId);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        System.out.println("onUnbind:" + intent);
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        System.out.println("onRebind:" + intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        i = 20;
+        System.out.println("onDestroy");
     }
 }
--- app/src/main/java/com/echo/eui/views/MainActivity.java
+++ app/src/main/java/com/echo/eui/views/MainActivity.java
@@ -1,8 +1,10 @@
 package com.echo.eui.views;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.*;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,17 +12,25 @@ import com.echo.eui.R;
 import com.echo.eui.service.CountService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button start,stop;
+    private Button start, stop, bind, unbind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("Activity process:" + android.os.Process.myPid());
+        System.out.println("Activity Thread:" + Thread.currentThread().getId());
+
         start = (Button) findViewById(R.id.start_service);
         stop = (Button) findViewById(R.id.stop_service);
+        bind = (Button) findViewById(R.id.bind_service);
+        unbind = (Button) findViewById(R.id.unbind_service);
+
         start.setOnClickListener(this);
         stop.setOnClickListener(this);
+        bind.setOnClickListener(this);
+        unbind.setOnClickListener(this);
     }
 
     @Override
@@ -33,6 +43,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 stopService(intent);
                 break;
+            case R.id.bind_service:
+                bindService(intent, conn, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(conn);
+                break;
         }
     }
+
+    ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            System.out.println("ComponentName:" + name + "   IBinder:" + service);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            System.out.println("ComponentName:" + name);
+        }
+    };
 }
